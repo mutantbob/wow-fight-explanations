b6792ddaaff5e1beed7fe1dcb58b3956d66c21cf
Robert Forsman <git@thoth.purplefrog.com>
2012-11-08 18:54:42 -0500

    Fix copy&paste mistake.

--- en/raids/mogushan-vaults/garajal.lua
+++ en/raids/mogushan-vaults/garajal.lua
@@ -2,7 +2,7 @@
 -- This work is made available under the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 license, http://creativecommons.org/licenses/by-nc-sa/3.0/
 
 
-SL(sid)
+function GSL(sid)
     local a={ EJ_GetSectionInfo(sid) }
     return a[9]
 end
