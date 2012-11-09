1a623e7d729eeb0bd3a894758dc835a76e752e8f
Robert Forsman <git@thoth.purplefrog.com>
2012-11-09 15:17:19 -0500

    Leave out some bits for LFR. Fix a typo.

--- en/raids/mogushan-vaults/5-elegon.lua
+++ en/raids/mogushan-vaults/5-elegon.lua
@@ -5,7 +5,13 @@ function GSL(sid)
    local a={ EJ_GetSectionInfo(sid) }
    return a[9]
 end
-EJ_SetDifficulty(1) -- 10 normal
+
+local d = 1;
+if "raid" == ({IsInInstance()})[2] then
+  d = GetRaidDifficulty()
+end
+EJ_SetDifficulty(d)
+
 
 
 local CelestialBreath = GSL(6184)
@@ -28,14 +34,14 @@ local c="raid"
 
 
 CHAT(c, "Only the tank should be in front of Elegon because of "..CelestialBreath..".")
-if true then
-CHAT(c, "When the "..CelestialProtector.." materializes, tanks swap.  The protector tank should taunt and run to the edge of the platform to ditch "..Overcharged.." stacks.")
-CHAT(c, "Make sure the protector is on the platform so he is not protected by "..Phasing..".")
-CHAT(c, "Once the protector's health is <25% kite it off the platform because "..TotalAnnihilation.." plus "..StabilityFlux.." enhanced by "..TouchTitans.." is deadly.")
-CHAT(c, "All DPS and healers should drop Overcharged stacks before Stability Flux.")
+if d ~= 5 then
+    CHAT(c, "When the "..CelestialProtector.." materializes, tanks swap.  The protector tank should taunt and run to the edge of the platform to ditch "..Overcharged.." stacks.")
+    CHAT(c, "Make sure the protector is on the platform so he is not protected by "..Phasing..".")
+    CHAT(c, "When protector's health is <25% kite it off the platform because "..TotalAnnihilation.." plus "..StabilityFlux.." enhanced by "..TouchTitans.." is deadly.")
+    CHAT(c, "All DPS and healers should drop Overcharged stacks before Stability Flux.")
 end
 CHAT(c, "After elegon casts "..DrawPower.." all players kill "..EnergyCharge.."s to stack a debuff on Elegon.  Waves of these keep coming.")
-CHAT(c, "Drop "..Overcharge.." stacks before killing the Energy Charge because "..Discharge.." hurts.")
+CHAT(c, "Drop "..Overcharged.." stacks before killing the Energy Charge because "..Discharge.." hurts.")
 CHAT(c, "Eventually "..HighEnergy.." will enable a charge to reach an "..EmpyrealFocus.." and the central platform will disappear, dropping any clueless players to their death.")
 CHAT(c, "Blast all 6 "..EmpyrealFocus.." to respawn the platform.  Destroy any "..CosmicSpark.."s and resume attacking the boss.")
 CHAT(c, "After the second cycle "..RadiatingEnergies.." will quickly kill any players who are slow to return to the center.")
