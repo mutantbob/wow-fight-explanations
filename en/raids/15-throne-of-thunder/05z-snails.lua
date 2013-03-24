02971a8c676f57e8c8f6e8f7f4cf886c0bed997c
Robert Forsman <git@thoth.purplefrog.com>
2013-03-24 17:12:59 -0400

    Quick warning for the Gastropods that Devour noobs.

---
+++ en/raids/15-throne-of-thunder/05z-snails.lua
@@ -0,0 +1,28 @@
+-- Copyright (c) 2012 Robert Forsman
+-- This work is made available under the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 license, http://creativecommons.org/licenses/by-nc-sa/3.0/
+
+
+function GSL(sid)
+   local a={ EJ_GetSectionInfo(sid) }
+   return a[9]
+end
+
+local d = 1;
+local c="SAY"
+if "raid" == ({IsInInstance()})[2] then
+  d = GetRaidDifficultyID()-2
+  c="RAID"
+end
+if (IsInGroup(LE_PARTY_CATEGORY_INSTANCE)) then
+  c="INSTANCE_CHAT"
+end
+
+EJ_SetDifficulty(d)
+
+local Devoured = GetSpellLink(134415)
+
+
+SendChatMessage("DO NOT MESS AROUND WITH DEATH SNAILS.  DEATH SNAILS WILL MAKE YOU DIE!", c)
+SendChatMessage("If you stand in front of the DEATH SNAIL you will be "..Devoured..".", c)
+SendChatMessage("Somebody will think I'm joking and they will DIE and realize that I AM NOT JOKING.", c)
+
