3fce6f46d0314ce753a7ed193f1a69e026480175
Robert Forsman <git@thoth.purplefrog.com>
2013-03-26 15:20:56 -0400

    Add a menu path. Add a bit about tanking and DPSing.

--- en/raids/15-throne-of-thunder/05z-snails.lua
+++ en/raids/15-throne-of-thunder/05z-snails.lua
@@ -1,5 +1,6 @@
 -- Copyright (c) 2012 Robert Forsman
 -- This work is made available under the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 license, http://creativecommons.org/licenses/by-nc-sa/3.0/
+-- ACmenu=raids/Throne of Thunder/death snails
 
 
 function GSL(sid)
@@ -23,6 +24,7 @@ local Devoured = GetSpellLink(134415)
 
 
 SendChatMessage("DO NOT MESS AROUND WITH DEATH SNAILS.  DEATH SNAILS WILL MAKE YOU DIE!", c)
-SendChatMessage("If you stand in front of the DEATH SNAIL you will be "..Devoured..".", c)
+SendChatMessage("If you stand in front of the DEATH SNAIL you will be "..Devoured..".  If you try to tank DEATH SNAIL you will be "..Devoured..".", c)
+SendChatMessage("Attack from ranged, or maybe from the side, but be ready for him to change target and direction", c)
 SendChatMessage("Somebody will think I'm joking and they will DIE and realize that I AM NOT JOKING.", c)
 
