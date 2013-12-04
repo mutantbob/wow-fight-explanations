c86d37844cdd899c3392e7267cf29cc0c11cc9cd
Robert Forsman <git@thoth.purplefrog.com>
2013-12-04 11:08:52 -0500

    Update for new cliche header.

--- en/raids/15-throne-of-thunder/01-jin-rokh.lua
+++ en/raids/15-throne-of-thunder/01-jin-rokh.lua
@@ -2,7 +2,6 @@
 -- This work is made available under the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 license, http://creativecommons.org/licenses/by-nc-sa/3.0/
 -- ACmenu=raids/Throne of Thunder/Jin'rokh
 
-
 function GSL(sid)
    local a={ EJ_GetSectionInfo(sid) }
    return a[9]
@@ -21,6 +20,8 @@ end
 if (IsInGroup(LE_PARTY_CATEGORY_INSTANCE)) then
   c="INSTANCE_CHAT"
 end
+
+lfr = ( d==5 )
 
 EJ_SetDifficulty(d)
 
